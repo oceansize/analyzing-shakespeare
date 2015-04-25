14df7754ca8c44d44c63e52239fcaaf2e2361f83
Roi Driscoll <roi.driscoll@gmail.com>
2015-04-25 19:28:32 +0100

    Renames class, adds Webmock & VCR

---
+++ spec/macbeth_analyzer_spec.rb
@@ -0,0 +1,9 @@
+require 'macbeth_analyzer'
+
+describe MacbethAnalyser do
+
+  it "can retrieve data from a url" do
+
+  end
+
+end
--- spec/play_parser_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'play_parser'
-
-describe PlayParser do
-
-  it "can retrieve data from a url" do
-
-  end
-
-end
